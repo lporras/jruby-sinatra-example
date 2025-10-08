bc6cca56ea21509abae2f4b85b22ead3b34846a7
Luis Porras <lporras16@gmail.com>
2025-10-07 22:33:34 -0500

    remove old hello world examples

--- timestamp_hello_world.rb
+++
@@ -1,9 +0,0 @@
-class Hola
-  def saludar
-    puts "Hola mundo desde Ruby!"
-    puts "La hora actual es: #{Time.now}"
-  end
-end
-
-Hola.new.saludar
-# Ejecuta este archivo con: ruby timestamp_hello_world.rb
